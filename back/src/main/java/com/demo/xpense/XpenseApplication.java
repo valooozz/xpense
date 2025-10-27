b8483100c065e8112e72b60898eee8caf1e06847
valooozz <valentin.corazza@ikmail.com>
2025-10-27 11:46:06 +0100

    dotenv commenté, pour déploiement

--- back/src/main/java/com/demo/xpense/XpenseApplication.java
+++ back/src/main/java/com/demo/xpense/XpenseApplication.java
@@ -3,17 +3,15 @@ package com.demo.xpense;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class XpenseApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		// Dotenv dotenv = Dotenv.load();
 
-        dotenv.entries().forEach(entry ->
-                System.setProperty(entry.getKey(), entry.getValue())
-        );
+        // dotenv.entries().forEach(entry ->
+        //         System.setProperty(entry.getKey(), entry.getValue())
+        // );
 		
 		SpringApplication.run(XpenseApplication.class, args);
 	}
