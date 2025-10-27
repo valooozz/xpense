afe04494d5b6a8da4cf8c981ff4acb1d2495e03d
valooozz <valentin.corazza@ikmail.com>
2025-10-27 11:36:33 +0100

    utilisation .env + Dockerfile

--- back/src/main/java/com/demo/xpense/XpenseApplication.java
+++ back/src/main/java/com/demo/xpense/XpenseApplication.java
@@ -3,10 +3,18 @@ package com.demo.xpense;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class XpenseApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+		
 		SpringApplication.run(XpenseApplication.class, args);
 	}
 
