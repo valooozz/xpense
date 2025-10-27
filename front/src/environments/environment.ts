9d13ca7cadd8093bcbc297bc68da121a37b50ab9
valooozz <valentin.corazza@ikmail.com>
2025-10-27 14:18:45 +0100

    url back dans le front pour deploiement

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api'
+    // apiUrl: 'http://localhost:8080/api'
+    apiUrl: 'https://xpense-backend-btw4.onrender.com'
 };
   
