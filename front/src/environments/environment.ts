39cac49f0386cb32dc61b8d14ab3888519538e0a
valooozz <valentin.corazza@ikmail.com>
2025-10-27 16:58:50 +0100

    config urls

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     // apiUrl: 'http://localhost:8080/api'
-    apiUrl: 'https://xpense-backend-btw4.onrender.com'
+    apiUrl: 'https://xpense-backend-btw4.onrender.com:8080/api'
 };
   
